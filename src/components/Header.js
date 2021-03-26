a387950820a6df21d1c15ce64397a79d9dd6e382
Petr <solodov.petr@gmail.com>
2021-03-27 01:46:41 +0200

    Header, navigation

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Nav, NavItem, NavLink, Navbar, NavbarBrand } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const Header  = () => {
+    return (
+        <>
+            <Navbar className="yellotail">
+                <LinkContainer to="/">
+                    <NavbarBrand>
+                        The Daily News
+                    </NavbarBrand>
+                </LinkContainer>
+            </Navbar>
+            <Nav>
+                <NavItem>
+                    <LinkContainer to="/">
+                        <NavLink>
+                            Home
+                        </NavLink>
+                    </LinkContainer>
+                </NavItem>
+                <NavItem>
+                    <LinkContainer to="/contact">
+                        <NavLink>
+                            Contact
+                        </NavLink>
+                    </LinkContainer>
+                </NavItem>
+            </Nav>
+        </>
+    );
+}
+
+export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
 import Home from './components/home';
 
 const Routes = () => (
     <BrowserRouter>
+        <Header />
         <Switch>
             <Route path="/" component={Home} exact />
         </Switch>
