c98bbfe0d093248d7df2567342076d38a9878b19
Petr <solodov.petr@gmail.com>
2021-03-28 06:17:35 +0300

    Render home posts with react-masonry-css and -moment

--- src/components/home/HomePosts.js
+++ src/components/home/HomePosts.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from '../../store/actions';
 import { Button, Spinner } from 'react-bootstrap';
+import Masonry from 'react-masonry-css';
+import Moment from 'react-moment';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const HomePosts = () => {
 
@@ -25,6 +28,7 @@ const HomePosts = () => {
     return (
         <>
             <h2>Home Posts</h2>
+
             { loading && (
                 <div style={{textAlign: 'center'}}>
                     <Spinner animation="border" role="status">
@@ -32,6 +36,38 @@ const HomePosts = () => {
                     </Spinner>
                 </div>
             ) }
+
+            { !loading && posts && (
+                <Masonry
+                    breakpointCols={{default: 3, 800: 2, 400: 1}}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column">
+                    {
+                        posts.map(post => {
+                            const { id, title, excerpt, image, author, createdAt } = post;
+                            return (
+                                <div key={id}>
+                                    <img style={{width: '100%', height: '200px'}} src={image} alt="" />
+                                    <div className="author">
+                                        <span>{author} -</span>
+                                        <Moment  format="DD MMMM Y">
+                                            {createdAt}
+                                        </Moment>
+                                    </div>
+                                    <div className="content">
+                                        <div className="title">{title}</div>
+                                        <div className="excerpt">{excerpt}</div>
+                                        <LinkContainer to={`/article/${id}`} className="mt-3">
+                                            <Button variant="light">Read more</Button>
+                                        </LinkContainer>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </Masonry>
+            ) }
+
             { !loading && morePosts && (
                 <Button variant="outline-dark"
                         onClick={loadMoreHandler}>
